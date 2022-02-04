dee22797b0ddafc470737b618c65e1e237185d23
Chinmay Parvate <chinmayparvate@gmail.com>
2022-02-04 20:56:25 +0530

    Add files via upload

---
+++ mysql_dockerfile/test.sql
@@ -0,0 +1,7 @@
+CREATE table studentData (Name varchar(25),RollNo integer,Address varchar(50),MobileNumber varchar(20),PanNumber varchar(20)); 
+
+INSERT into studentData values('Boom' ,20112028 ,'Palermo' ,'7977835891' ,'IASJHIDHJ1512');
+INSERT into studentData values('Boom' ,20112025 ,'Nairobi' ,'9845136145' ,'IHAFOSIH83838');
+INSERT into studentData values('Ciao' ,20112023 ,'Tokyo' ,'9912214502' ,'PALLFGGO28747');
+INSERT into studentData values('Puta' ,20112023 ,'Berlin' ,'9912214502' ,'VOHBVBOO388');
+INSERT into studentData values('Madre' ,20112023 ,'Professor' ,'9912214502' ,'BLAKDO29393');
